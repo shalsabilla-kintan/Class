15f82300d55d9d6804e423220720f5597732f116
Shalsabilla Kintan <shalsakintan@gmail.com>
2022-04-11 13:26:49 +0700

    commit

---
+++ 5_Basic Dart Programming/praktikum.dart
@@ -0,0 +1,9 @@
+void main (){
+  int r = 10;
+  double phi = 3.14;
+
+  var hasil = 
+  (phi*r*r);
+  print('luas lingkaran denga jari-jari 10 adalah :' +
+  hasil.toString());
+}
---
+++ 5_Basic Dart Programming/prakttikum.string.dart
@@ -0,0 +1,7 @@
+void main (){
+  String kebutuhanprimer = 'makanan';
+  String kebutuhansekunder = 'pakaian';
+  String kebutuhantersier = 'liburan';
+
+  print(kebutuhanprimer + kebutuhansekunder + kebutuhantersier);
+}
